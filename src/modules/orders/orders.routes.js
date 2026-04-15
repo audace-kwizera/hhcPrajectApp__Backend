55ef3910f77e7335e5666de1800fc7ce735cd378
Kwizera Audace <audace@MacBook-Pro-3.local>
2026-04-15 18:36:04 +0200

    secure commande

--- src/modules/orders/orders.routes.js
+++ src/modules/orders/orders.routes.js
@@ -2,19 +2,35 @@ const express = require("express");
 const router = express.Router();
 const service = require("./orders.service");
 const auth = require("../../middlewares/auth.middleware");
+const checkRole = require("../../middlewares/role.middleware");
 
-router.post("/", auth, async (req, res) => {
-  try {
-    const order = await service.createOrder(req.body);
-    res.json(order);
-  } catch (err) {
-    res.status(400).json({ error: err.message });
+
+// 🔥 CREATE ORDER → EMPLOYEE / PARTNER (POS)
+router.post(
+  "/",
+  auth,
+  checkRole("EMPLOYEE", "PARTNER"),
+  async (req, res) => {
+    try {
+      const order = await service.createOrder(req.body);
+      res.json(order);
+    } catch (err) {
+      res.status(400).json({ error: err.message });
+    }
   }
-});
+);
 
-router.get("/", auth, async (req, res) => {
-  const data = await service.getOrders();
-  res.json(data);
-});
+
+// 🔥 GET ALL ORDERS → ADMIN ONLY
+router.get(
+  "/",
+  auth,
+  checkRole("ADMIN"),
+  async (req, res) => {
+    const data = await service.getOrders();
+    res.json(data);
+  }
+);
+
 
 module.exports = router;
